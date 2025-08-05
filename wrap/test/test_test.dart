b8ce0d39208c34f9bde85a63e6d2d6d36e80e91a
James Aprosail <aprosail@outlook.com>
2025-08-05 23:23:47 +0800

    feat: ensure text and basic wraps

---
+++ wrap/test/test_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:wrap/test.dart';
+import 'package:wrap/wrap.dart';
+
+void main() {
+  testWidgets('ensure text', (t) async {
+    const message = 'message';
+    await t.pumpWidget(message.asText().center().ensureText());
+    expect(find.text(message), findsOneWidget);
+  });
+}
